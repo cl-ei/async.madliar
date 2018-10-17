df23deb2a671e67365392f3d322a3e41995ba4e7
Caoliang <i@caoliang.net>
2018-10-17 18:06:12 +0800

    only record 500 error.

--- app/middleware.py
+++ app/middleware.py
@@ -8,12 +8,12 @@ async def default_middle_ware(app, handler):
         try:
             response = await handler(request)
         except Exception as e:
-            error_message = str(e)
-            traceback_info = traceback.format_exc()
-            logging.error("Error happend: %s\n%s\n" % (error_message, traceback_info))
-
             status_code = getattr(e, "status_code", 500)
             reason = getattr(e, "reason", "Internal Server Error")
+            if status_code == 500:
+                error_message = str(e)
+                traceback_info = traceback.format_exc()
+                logging.error("Error happend: %s\n%s\n" % (error_message, traceback_info))
             content = "<center><h3>%s %s!</h3></center>" % (status_code, reason)
             response = HttpResponse(content, status=status_code, reason=reason)
         response.headers.add("Server", "madliar")
