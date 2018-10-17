6e7a85c4d83f01823cc25f426b77bc107eb0229b
Caoliang <i@caoliang.net>
2018-10-17 17:05:42 +0800

    add gen script.

--- etc/config.py
+++ etc/config.py
@@ -1,5 +1,6 @@
 PROJECT_ROOT = "./"
 
+POST_ARTICLE_PATH = "templates/_post/article"
 PARSED_ARTICLE_JSON = "../madliar.com/static/blog/article"
 PARSED_ARTICLE_JSON_PATH = "static/blog/article"
 CDN_URL = ""
