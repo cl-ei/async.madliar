424b5e2dc11f88e89d9c88fc7b2f6808e8d07980
Caoliang <i@caoliang.net>
2018-11-26 21:14:14 +0800

    new sender.

--- hansy.py
+++ hansy.py
@@ -1,6 +1,15 @@
 import json
 import requests
 import time
+import redis
+
+
+REDIS_CONFIG = {
+    "host": "47.104.176.84",
+    "port": 19941,
+    "password": "redispassword",
+    "db": 8
+}
 
 
 def send_danmaku(msg, roomid=4424139, color=0xffffff):
@@ -33,11 +42,27 @@ def send_danmaku(msg, roomid=4424139, color=0xffffff):
     return not (r.status_code != 200 or json.loads(r.content.decode("utf-8")).get("code") != 0)
 
 
-if __name__ == "__main__":
+def main():
+    r = redis.Redis(**REDIS_CONFIG)
+    try:
+        time_sec = int(r.get("HANSY_TIME"))
+        index = int(r.get("HANSY_INDEX"))
+    except Exception:
+        return
+
+    if int(time.time()) - time_sec > 70:
+        return
+
+    index += 1
+    r.set("HANSY_INDEX", index % 3)
+
     msg_list = [
         "各位小可爱记得点上关注哟，点个关注不迷路 ヽ(✿ﾟ▽ﾟ)ノ",
         "喜欢泡泡的小伙伴，加粉丝QQ群436496941来撩骚呀~",
         "更多好听的原创歌和翻唱作品，网易云音乐搜索「管珩心」~",
     ]
-    for msg in msg_list:
-        print(send_danmaku(msg, roomid=4424139, color=16738408))
+    send_danmaku(msg_list[index], roomid=4424139)
+
+
+if __name__ == "__main__":
+    main()
