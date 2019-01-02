c7cb6770cda6d0f81f7e3433fbf4447c6a8ee4ef
Caoliang <i@caoliang.net>
2019-01-02 16:07:18 +0800

    rebuild.

--- etc/config.py
+++ etc/config.py
@@ -1,7 +1,7 @@
 import sys
 DEBUG = bool(sys.platform in ("win32", "darwin"))
 
-PROJECT_ROOT = "./" if DEBUG else "/home/wwwroot/async.madliar.com"
+PROJECT_ROOT = "./" if DEBUG else "/home/wwwroot/async.madliar"
 LOG_PATH = "./log" if DEBUG else "/home/wwwroot/log/async.madliar"
 MUSIC_FOLDER = "./music" if DEBUG else "/home/wwwroot/statics/music"
 RAW_ARTICLE_PATH = "templates/_post/article"
