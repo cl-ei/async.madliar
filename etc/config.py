d4aaee9f364314832db9d287c62ab742378a04e9
Caoliang <i@caoliang.net>
2019-06-17 11:24:42 +0800

    update render.

--- etc/config.py
+++ etc/config.py
@@ -1,10 +1,22 @@
 import sys
+import configparser
+
+print("-"*80)
+
+
 DEBUG = bool(sys.platform in ("win32", "darwin"))
+
+config = configparser.ConfigParser()
+config.read('/etc/madliar.settings.ini')
+
+try:
+    CDN_URL = config["default"]["CDN_URL"]
+except KeyError:
+    CDN_URL = "https://statics.madliar.com"
+
 
 PROJECT_ROOT = "./" if DEBUG else "/home/wwwroot/async.madliar"
 LOG_PATH = "./log" if DEBUG else "/home/wwwroot/log/async.madliar"
 MUSIC_FOLDER = "./music" if DEBUG else "/home/wwwroot/statics/music"
 RAW_ARTICLE_PATH = "templates/_post/article"
 DIST_ARTICLE_PATH = "./dist_article" if DEBUG else "/home/wwwroot/statics/dist_article"
-CDN_URL = "https://statics.madliar.com"
-
