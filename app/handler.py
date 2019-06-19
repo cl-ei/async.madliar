e351e55488c2fc1e4debaa969426a448dacd7686
Caoliang <i@caoliang.net>
2019-06-19 17:30:07 +0800

    update thank.

--- app/handler.py
+++ app/handler.py
@@ -1,20 +1,9 @@
 import os
-import sys
 import json
-import copy
-import datetime
 import asyncio
-import random
-import requests
-import time
 from app.http import HttpResponse, render_to_response
-from etc import (
-    DIST_ARTICLE_PATH,
-    CDN_URL,
-    MUSIC_FOLDER,
-    DEBUG,
-)
-from etc.log4 import logging
+from etc import DIST_ARTICLE_PATH, CDN_URL, MUSIC_FOLDER, DEBUG
+from app.lt import LtOperations
 
 
 async def robots_response(request):
@@ -52,10 +41,12 @@ async def index(request):
         if f.lower().endswith(".js"):
             article_js_file_name = f
             break
+
     if DEBUG:
         article_js_link = os.path.join(DIST_ARTICLE_PATH, article_js_file_name)
     else:
         article_js_link = CDN_URL + "/dist_article/" + article_js_file_name
+
     context = {
         "article_js_link": article_js_link,
         "page": {
@@ -71,180 +62,8 @@ async def index(request):
     )
 
 
-async def thank(req):
-    get_list = req.query.get("get_gift_list")
-    if get_list:
-        return await get_gift_list(req)
-
-    bg_color = req.query.get("bg_color")
-    today_lines = req.query.get("today_lines", 3)
-    demo = int(req.query.get("demo", 0))
-    scale = float(req.query.get("scale", 0.65))
-    margin_top = int(525 / 2 * (1 - scale))
-    margin_left = int(522 / 2 * (1 - scale))
-
-    with open("../stormgift/data/hansy_guard_list.txt", "rb") as f:
-        guard_text = f.read().decode("utf-8")
-
-    context = {
-        "today_lines": today_lines,
-        "bg_color": bg_color,
-        "demo": demo,
-        "scale": "%.2f" % scale,
-        "margin_top": margin_top,
-        "margin_left": margin_left,
-        "guard_text": guard_text,
-    }
-    return render_to_response("templates/thank_v.html", context=context)
-
-
-def _download_deficiency_face(uid):
-    req_url = f"https://api.bilibili.com/x/space/acc/info?jsonp=jsonp&mid={uid}"
-    headers = {
-        "Accept": (
-            "text/html,application/xhtml+xml,application/xml;"
-            "q=0.9,image/webp,image/apng,*/*;q=0.8"
-        ),
-        "User-Agent": (
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) "
-            "AppleWebKit/537.36 (KHTML, like Gecko) "
-            "Chrome/70.0.3538.110 Safari/537.36"
-        ),
-    }
-    try:
-        r = requests.get(req_url, headers=headers, timeout=10)
-        face = json.loads(r.text).get("data", {}).get("face", "") or ""
-        if not face:
-            raise RuntimeError("Bad face url")
-    except Exception as e:
-        logging.error(f"Cannot get user face, uid -> {uid}, e: {e}")
-        return
-
-    try:
-        r = requests.get(face, timeout=30)
-        if r.status_code != 200:
-            raise Exception("Request error when get face!")
-        if "linux" in sys.platform:
-            filename = f"/home/wwwroot/statics/static/face/{uid}"
-        else:
-            filename = f"./temp_data/{uid}"
-        with open(filename, "wb") as f:
-            f.write(r.content)
-    except Exception as e:
-        logging.error(f"Cannot save face, e: {e}, {uid} -> {face}")
-        return
-
-    logging.info(f"User face saved, {uid} -> {face}")
-    return True
-
-
-HISTORY_DISPLAY_GIFTS = (
-    "王冠小电视", "小电视飞船",
-    "幻乐之声", "花之少女", "DokiDoki", "C位光环", "摩天大楼",
-    "琴语", "天空之翼",
-    "节奏风暴",
-)
-
-
-async def get_gift_list(req):
-    file_name = "../stormgift/data/hansy_gift_list.txt"
-    version = f"{datetime.date.today()}-{str(os.path.getmtime(file_name))}"
-    if version == req.query.get("version"):
-        return HttpResponse(json.dumps({"version": version}))
-
-    with open(file_name, "r", encoding="utf-8") as f:
-        gift_list = f.readlines()
-
-    try:
-        existed_uid_list = os.listdir("/home/wwwroot/statics/static/face")
-    except Exception:
-        existed_uid_list = []
-
-    history = []
-    today = []
-    today_str = f"{datetime.date.today()}"
-    deficiency_face = False
-    download_deficiency_face_start_time = time.time()
-    for line in gift_list:
-        try:
-            data = json.loads(line)
-            created_time = data["created_time"]
-
-            uid = data["uid"]
-            sender = data["sender"]
-            gift_name = data["gift_name"]
-            count = data["count"]
-            if str(uid) in existed_uid_list:
-                face = f"https://statics.madliar.com/static/face/{uid}"
-            else:
-                cost_time = time.time() - download_deficiency_face_start_time
-                if cost_time < 40 and _download_deficiency_face(uid) is True:
-                    face = f"https://statics.madliar.com/static/face/{uid}"
-                else:
-                    face = f"https://statics.madliar.com/static/face/default"
-                    deficiency_face = True
-
-            gift_img = f"https://statics.madliar.com/static/gift/{data['gift_name']}"
-
-            if created_time[:10] == today_str:
-                today.append({
-                    "sender": sender,
-                    "face": face,
-                    "count": count,
-                    "send_text": "赠送",
-                    "gift_name": gift_name,
-                    "gift_img": gift_img,
-                })
-            elif created_time[:7] == today_str[:7]:
-                history.append({
-                    "sender": sender,
-                    "face": face,
-                    "count": count,
-                    "send_text": "赠送",
-                    "gift_name": gift_name,
-                    "gift_img": gift_img,
-                })
-        except Exception as e:
-            pass
-
-    if not today:
-        today.append({
-            "sender": "管珩心",
-            "face": "https://statics.madliar.com/static/face/65568410",
-            "gift_name": "不负你的深情",
-        })
-
-    packaged_history = []
-    for g in history:
-        if g["gift_name"] not in HISTORY_DISPLAY_GIFTS:
-            continue
-
-        for d in packaged_history:
-            if d["sender"] == g["sender"] and d["gift_name"] == g["gift_name"]:
-                d["count"] += g["count"]
-                break
-        else:
-            packaged_history.append(copy.deepcopy(g))
-
-    if not packaged_history:
-        packaged_history = [{
-            "sender": "管珩心",
-            "face": "https://statics.madliar.com/static/face/65568410",
-            "gift_name": "不负你的深情",
-        }]
-    else:
-        packaged_history.sort(key=lambda x: (HISTORY_DISPLAY_GIFTS.index(x["gift_name"]), -x["count"]))
-
-    # Do not force update.
-    # if deficiency_face:
-    #     version += str(random.random())[:10]
-
-    data = {
-        "history": packaged_history,
-        "today": today,
-        "version": version,
-    }
-    return HttpResponse(json.dumps(data))
+async def thank(request):
+    return await LtOperations.thank(request)
 
 
 async def music_response(request):
--- etc/log4.py
+++ etc/log4.py
@@ -1,40 +1,15 @@
 import os
 import sys
+import logging
 from etc.config import LOG_PATH
 
-__all__ = ("logging", )
 
-
-def __make_logger(name, log_file_name, level="DEBUG", log_format=None):
-    import logging
-
-    level_names = {
-        'CRITICAL': logging.CRITICAL,
-        'ERROR': logging.ERROR,
-        'WARN': logging.WARNING,
-        'WARNING': logging.WARNING,
-        'INFO': logging.INFO,
-        'DEBUG': logging.DEBUG,
-        'NOTSET': logging.NOTSET,
-    }
-
-    fh = logging.FileHandler(os.path.join(LOG_PATH, log_file_name))
-
-    if log_format is None:
-        log_format = '%(levelname)s %(asctime)s %(filename)s:%(lineno)d:%(funcName)s %(message)s'
-    fh.setFormatter(logging.Formatter(log_format))
-
-    console = logging.StreamHandler(sys.stdout)
-    console.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s]: %(message)s"))
-
-    logger = logging.getLogger(name)
-    logger.setLevel(level_names.get(level.upper(), logging.DEBUG))
-    logger.addHandler(fh)
-    logger.addHandler(console)
-    return logger
-
-
-logging = __make_logger(
-    name="web_syslog",
-    log_file_name="web_syslog.log",
-)
+log_format = logging.Formatter("%(asctime)s [%(levelname)s]: %(message)s")
+console = logging.StreamHandler(sys.stdout)
+console.setFormatter(log_format)
+file_handler = logging.FileHandler(os.path.join(LOG_PATH, "madliar.log"))
+file_handler.setFormatter(log_format)
+madliar_logger = logging.getLogger("madliar")
+madliar_logger.setLevel(logging.DEBUG)
+madliar_logger.addHandler(console)
+madliar_logger.addHandler(file_handler)
