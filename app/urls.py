8eab304ddd40f638c6f0c3b0a21710a5f565db85
Caoliang <i@caoliang.net>
2019-11-20 10:29:59 +0800

    add lock-scroll.

--- app/urls.py
+++ app/urls.py
@@ -28,7 +28,7 @@ user_url_map = (
     ("get", r"/console", console),
     ("get", r"/grafana", grafana),
     ("get", r"/bili_live", bili_live),
-    ("post", r"/log", log),
+    (("post", "get"), r"/log", log),
     ("post", r"/register_clserver", register_clserver),
 )
 
