7b67ce1e6174a8fc1d0d61682005d97d1319e0dc
lianglcao <lianglcao@tencent.com>
2022-07-12 14:44:03 +0800

    add dockerfile

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -5,7 +5,8 @@ DOCKER_IMAGE="async_madliar_img"
 docker stop ${DOCKER_NAME} 2> /dev/null
 docker rm ${DOCKER_NAME} 2> /dev/null
 
-docker run -itd --rm \
+docker run -itd \
+  --restart=always \
   --name ${DOCKER_NAME} \
   --net=host \
   ${DOCKER_IMAGE}
