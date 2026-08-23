77f964e55f7227c9c0ab64c1149382165b365431
caoliang <liang.cao.cn@gmail.com>
2026-08-23 22:29:34 +0800

    refine

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from src.worker.entry import start, stop
 
 if __name__ == "__main__":
     start()
-    uvicorn.run("src.main:app", port=10090, workers=1, server_header=False)
+    uvicorn.run("src.main:app", host="0.0.0.0", port=10090, workers=1, server_header=False)
     stop()
